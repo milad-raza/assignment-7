fd797c8d9cc128c0b3067e445ab1b989bac41a53
Milad Raza <miladrazabhatti@gmail.com>
2020-06-25 14:07:03 +0500

    Calculator

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ function fullresult(){
     var onclick = document.getElementById("onclick");
     onclick.value = eval(onclick.value)
 }
+function value4(){
+    var onclick = document.getElementById("onclick");
+    onclick.value = onclick.value.slice(0,-1)
+}
+function value5(){
+    var onclick = document.getElementById("onclick");
+    onclick.value = Math.pow(onclick.value,2)
+}
